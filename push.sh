4ed1fd36b57c9a56bbe55fb3fe65745c87ec7149
Unkn0wn0ne <nobody.known21@gmail.com>
2014-04-04 21:29:27 -0400

    Created push.sh script for telnet replacement
    I have to play around with telnet a little more to see if I can work. If not the roothandler will simply call this script. It uses the busybox path so hopefully I won't have any path issues.

---
+++ push.sh
@@ -0,0 +1,10 @@
+#!/system/bin/sh
+#Valet root script by Unkn0wn0ne
+su
+/system/bin/busybox mount -o rw, remount /system
+#Testing - remove at later date
+/system/bin/busybox mount -o rw, remount /
+/system/bin/busybox cp /data/local/tmp/su /system/xbin/su
+/system/bin/busybox ln -s /system/xbin/su /system/bin/su
+#Testing - remove at later date
+/system/bin/busybox echo Root test completed > /data/local/tmp/test.txt
