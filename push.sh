0fcc632003f14bf26bb3f67601b8af8c198c1364
Unkn0wn0ne <nobody.known21@gmail.com>
2014-04-07 22:00:27 -0400

    Removed root remount test from push.sh
    Causes weird issue

--- push.sh
+++ push.sh
@@ -2,8 +2,6 @@
 #Valet root script by Unkn0wn0ne
 /system/bin/busybox su
 /system/bin/busybox mount -o rw, remount /system
-#Testing - remove at later date
-/system/bin/busybox mount -o rw, remount /
 /system/bin/busybox cp /data/local/tmp/su /system/xbin/su
 /system/bin/busybox ln -s /system/xbin/su /system/bin/su
 #Testing - remove at later date
