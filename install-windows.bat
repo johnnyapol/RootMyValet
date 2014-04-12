926a1531c36939dfeb57385517c6ae8cad5dd747
Unkn0wn0ne <nobody.known21@gmail.com>
2014-04-12 13:47:18 -0400

    Create install-windows.bat
    Partially handles #2, will need a linux installer soon
    Requires the ANDROID_HOME variable be set to an installed SDK

---
+++ install-windows.bat
@@ -0,0 +1,21 @@
+@Echo off
+echo RootMyValet installer v1.0 by Unkn0wn0ne
+echo Please read the README before using this program
+echo RootMyValet Developers ARE NOT RESPONSIBLE FOR ANYTHING THE HAPPENS DURING THE USAGE OF THIS PROGRAM.
+echo Killing ADB server
+%ANDROID_HOME%\platform-tools\adb kill-server
+echo Installing RootMyValet
+echo Please connect device to computer and hit enter
+pause
+%ANDROID_HOME%\platform-tools\adb wait-for-device
+%ANDROID_HOME%\platform-tools\adb push su /data/local/tmp
+%ANDROID_HOME%\platform-tools\adb push roothandler /data/local/tmp
+%ANDROID_HOME%\platform-tools\adb push push.sh /data/local/tmp
+%ANDROID_HOME%\platform-tools\adb shell chmod 755 /data/local/tmp/push.sh
+%ANDROID_HOME%\platform-tools\adb shell chmod 0755 /data/local/tmp/roothandler
+%ANDROID_HOME%\platform-tools\adb install RootMyValet.apk
+echo Rebooting device...
+%ANDROID_HOME%\platform-tools\adb reboot
+%ANDROID_HOME%\platform-tools\adb wait-for-device
+echo Thank you for using RootMyValet, your device should now be rooted. If you are using a ZTE Valet phone, you cannot delete the application or it's files or the root will go away.
+pause
