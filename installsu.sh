1f8a115cbc03debd8ffb5e39eda927101716912d
Unkn0wn0ne <nobody.known21@gmail.com>
2014-04-17 09:50:57 -0400

    Added a modified version of installsu.sh, credits to karmmisht and mainefungi for this

---
+++ installsu.sh
@@ -0,0 +1,19 @@
+#!/system/bin/sh
+# telnet in and install su
+ 
+( \
+echo 'stty -echo'; sleep 2; \
+echo 'su'; sleep 2; \
+echo 'exit'; sleep 2; \
+echo 'mount -o rw,remount /system'; sleep 2; \
+echo 'cp /sdcard/su /system/xbin/su'; sleep 2; \
+echo 'chmod 06755 /system/xbin/su'; sleep 2; \
+echo 'ln -s /system/xbin/su /system/bin/su'; sleep 2; \
+echo 'exit'; sleep 2 ) \
+ | busybox telnet 127.0.0.1 23
+ 
+if [ -f /system/xbin/su ]; then
+    echo "\nsu successfully copied into /system/xbin"
+else
+    echo "\nfailed to copy su into /system/xbin"
+fi
--- push.sh
+++
@@ -1,8 +0,0 @@
-#!/system/bin/sh
-#Valet root script by Unkn0wn0ne
-/system/bin/busybox su
-/system/bin/busybox mount -o rw, remount /system
-/system/bin/busybox cp /data/local/tmp/su /system/xbin/su
-/system/bin/busybox ln -s /system/xbin/su /system/bin/su
-#Testing - remove at later date
-/system/bin/busybox echo Root test completed > /data/local/tmp/test.txt
