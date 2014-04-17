5da5acb6b13aefd1bad809b9102f2df5dcbe7a30
Unkn0wn0ne <nobody.known21@gmail.com>
2014-04-17 09:55:18 -0400

    Update installsu.sh

--- installsu.sh
+++ installsu.sh
@@ -1,5 +1,7 @@
 #!/system/bin/sh
 # telnet in and install su
+# karmmisht and mainefungi
+# Modified by Unkn0wn0ne for use in RootMyValet
  
 ( \
 echo 'stty -echo'; sleep 2; \
@@ -9,6 +11,7 @@ echo 'mount -o rw,remount /system'; sleep 2; \
 echo 'cp /sdcard/su /system/xbin/su'; sleep 2; \
 echo 'chmod 06755 /system/xbin/su'; sleep 2; \
 echo 'ln -s /system/xbin/su /system/bin/su'; sleep 2; \
+echo 'pkill telnetd'; sleep 2; \
 echo 'exit'; sleep 2 ) \
  | busybox telnet 127.0.0.1 23
  
