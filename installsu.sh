ddd7a2105a10f402524e38da113b024998d96ccd
John <nobody.known21@gmail.com>
2014-05-12 19:45:39 -0400

    Fix install.su Partially handles #6

--- installsu.sh
+++ installsu.sh
@@ -1,19 +1,17 @@
 #!/system/bin/sh
 # This here by Unkn0wn0ne for use in RootMyValet
 echo RootMyValet root script for Android devices
-/data/local/tmp/roothandler
 # karmmisht and mainefungi
 # Slightly modified by Unkn0wn0ne for use in root my valet
 ( \
-echo 'stty -echo'; sleep 2; \
-echo 'su'; sleep 2; \
-echo 'exit'; sleep 2; \
-echo 'mount -o rw,remount /system'; sleep 2; \
-echo 'cp /sdcard/su /system/xbin/su'; sleep 2; \
-echo 'chmod 06755 /system/xbin/su'; sleep 2; \
-echo 'ln -s /system/xbin/su /system/bin/su'; sleep 2; \
-echo 'pkill telnetd'; sleep 2; \
-echo 'exit'; sleep 2 ) \
+echo 'busybox stty -echo'; sleep 2; \
+echo 'busybox su'; sleep 2; \
+echo 'busybox mount -o rw,remount /system'; sleep 2; \
+echo 'busybox cp /sdcard/su /system/xbin/su'; sleep 2; \
+echo 'busybox chmod 06755 /system/xbin/su'; sleep 2; \
+echo 'busybox ln -s /system/xbin/su /system/bin/su'; sleep 2; \
+echo 'busybox pkill telnetd'; sleep 2; \
+echo 'busybox exit'; sleep 2 ) \
  | busybox telnet 127.0.0.1 23
  
 if [ -f /system/xbin/su ]; then
