6c9b9c2014671db81d0570cc486cdcdca2d5edea
John <nobody.known21@gmail.com>
2014-06-02 21:21:40 -0400

    Added version number to script for debugging

--- installsu.sh
+++ installsu.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
 # This here by Unkn0wn0ne for use in RootMyValet
-echo RootMyValet root script for Android devices
+echo RootMyValet root script for Android devices (v1.0.1)
 # karmmisht and mainefungi
 # Slightly modified by Unkn0wn0ne for use in root my valet
 ( 
