39826c5ebcdc2e288f3f3e598794b4bba7d25245
Elliot Huffman <thelich2@gmail.com>
2014-06-07 15:25:29 -0400

    New way to execute bulk telnet commands

--- installsu.sh
+++ installsu.sh
@@ -1,17 +1,18 @@
 #!/system/bin/sh
 # This here by Unkn0wn0ne for use in RootMyValet
-echo RootMyValet root script for Android devices v1.0.2
+echo RootMyValet root script for Android devices v1.0.3
 # karmmisht and mainefungi
 # Slightly modified by Unkn0wn0ne for use in root my valet
-(
-echo '/system/bin/busybox stty -echo'; sleep 2; 
-echo '/system/bin/busybox su'; sleep 2; 
-echo '/system/bin/busybox busybox mount -o rw,remount /system'; sleep 2; 
-echo '/system/bin/busybox busybox cp /sdcard/su /system/xbin/su'; sleep 2; 
-echo '/system/bin/busybox busybox chmod 06755 /system/xbin/su'; sleep 2; 
-echo '/system/bin/busybox ln -s /system/xbin/su /system/bin/su'; sleep 2; 
-echo '/system/bin/busybox pkill telnetd'; sleep 2) | /system/bin/busybox telnet 127.0.0.1 23
- 
+/system/bin/busybox telnet 127.0.0.1 23 <<EOF
+/system/bin/busybox stty -echo ; sleep 2; 
+/system/bin/busybox su ; sleep 2; 
+/system/bin/busybox busybox mount -o rw,remount /system ; sleep 2; 
+/system/bin/busybox busybox cp /sdcard/su /system/xbin/su ; sleep 2; 
+/system/bin/busybox busybox chmod 06755 /system/xbin/su ; sleep 2; 
+/system/bin/busybox ln -s /system/xbin/su /system/bin/su ; sleep 2; 
+/system/bin/busybox pkill telnetd ; sleep 2 
+EOF
+
 if [ -f /system/xbin/su ]; then
     echo "\nsu successfully copied into /system/xbin"
 else
