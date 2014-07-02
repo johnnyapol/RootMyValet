99bf2f0b0871d7f7e825f513739915138553702c
jjfriede <jjfriede@users.noreply.github.com>
2014-07-01 20:51:07 -0500

    Minor update to installsu.sh
    The old version worked but i coded it sloppy. I fixed duplicate calls to busybox (ex: '/system/bin/busybox busybox su' is now just 'busybox su'). Also, It is much easier to follow if it ever needed to be modified in the future.

--- installsu.sh
+++ installsu.sh
@@ -1,9 +1,18 @@
 #!/system/bin/sh
 # This here by Unkn0wn0ne for use in RootMyValet
-echo RootMyValet root script for Android devices v1.0.4
+echo RootMyValet root script for Android devices v1.0.5
 # karmmisht and mainefungi
 # Slightly modified by Unkn0wn0ne for use in root my valet
-(/system/bin/busybox stty -echo ; /system/bin/busybox su ; /system/bin/busybox busybox mount -o rw,remount /system ; /system/bin/busybox busybox cp /data/local/tmp/su /system/xbin/su ; /system/bin/busybox busybox chmod 06755 /system/xbin/su ; /system/bin/busybox ln -s /system/xbin/su /system/bin/su ; /system/bin/busybox pkill telnetd) | busybox telnet 127.0.0.1 23
+# Modified by jjfriede
+( 
+    busybox su ;
+    busybox mount -o rw,remount /system ;
+    busybox cp /data/local/tmp/su /system/xbin/su ;
+    busybox chmod 06755 /system/xbin/su ;
+    busybox ln -s /system/xbin/su /system/bin/su ;
+    busybox pkill telnetd
+) | busybox telnet 127.0.0.1 23
+
 if [ -f /system/xbin/su ]; then
     echo "\nsu successfully copied into /system/xbin"
 else
