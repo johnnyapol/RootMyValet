4c5e1f7745ef4953053b30ab5861e296d0f7ed8b
John <nobody.known21@gmail.com>
2014-09-27 21:18:33 -0400

    Update installsu.sh

--- installsu.sh
+++ installsu.sh
@@ -1,9 +1,11 @@
 #!/system/bin/sh
+
 # This here by Unkn0wn0ne for use in RootMyValet
-echo RootMyValet root script for Android devices v1.0.5
 # karmmisht and mainefungi
 # Slightly modified by Unkn0wn0ne for use in root my valet
 # Modified by jjfriede
+
+echo RootMyValet root script for Android devices v1.0.6
 ( 
     busybox su ;
     busybox mount -o rw,remount /system ;
@@ -12,9 +14,4 @@ echo RootMyValet root script for Android devices v1.0.5
     busybox ln -s /system/xbin/su /system/bin/su ;
     busybox pkill telnetd
 ) | busybox telnet 127.0.0.1 23
-
-if [ -f /system/xbin/su ]; then
-    echo "\nsu successfully copied into /system/xbin"
-else
-    echo "\nfailed to copy su into /system/xbin"
-fi
+echo Done.
