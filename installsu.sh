01a812271c2686f6e8d15be057e8952d481870a3
John <nobody.known21@gmail.com>
2014-09-27 21:28:24 -0400

    installsu.sh fixes

---
+++ installsu.sh
@@ -0,0 +1,18 @@
+# RootMyValet project
+
+# Thanks to karmmisht and mainefungi along with jjfried for their work
+
+echo RootMyValet root script for Android devices v1.1
+echo By karmmisht, mainefungi, jjfried.
+
+( 		
+    busybox su ;		
+    busybox mount -o rw,remount /system ;		
+    busybox cp /data/local/tmp/su /system/xbin/su ;		
+    busybox chmod 06755 /system/xbin/su ;		
+    busybox ln -s /system/xbin/su /system/bin/su ;		
+    busybox pkill telnetd		
+) | busybox telnet 127.0.0.1 23		
+
+echo Done.
+
